4ea3620fb9c87d86d38ed10e780d8939da804f6b
Kamil Popielarz <139891722+KamilPopielarz@users.noreply.github.com>
2023-08-12 13:04:40 +0200

    added total price calculation

--- java-project/src/Main.java
+++ java-project/src/Main.java
@@ -2,7 +2,9 @@ import service.PizzaPriceCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        PizzaPriceCalculator pizzaPriceCalculator = new PizzaPriceCalculator(0.04);
+        PizzaPriceCalculator pizzaPriceCalculator = new PizzaPriceCalculator(0.04, 3.51, 0.05);
         System.out.println("Pizza costs: " + pizzaPriceCalculator.calculatePrice(30) + " zł");
+        System.out.println("Delivery costs: " + pizzaPriceCalculator.calculateDeliveryPrice(10) + "zł");
+        System.out.println("OverallPrice: " + pizzaPriceCalculator.calculateTotalPrice(30, 10) + "zł");
     }
 }
--- java-project/src/service/PizzaPriceCalculator.java
+++ java-project/src/service/PizzaPriceCalculator.java
@@ -2,9 +2,13 @@ package service;
 
 public class PizzaPriceCalculator {
     private double priceForCm2;
+    private double pricePerKilometer;
+    private double tipPercentage;
 
-    public PizzaPriceCalculator(double priceForCm2FromConstructor) {
+    public PizzaPriceCalculator(double priceForCm2FromConstructor, double pricePerKilometerFromConstructor, double tipPercentageFromConstructor) {
         this.priceForCm2 = priceForCm2FromConstructor;
+        this.pricePerKilometer = pricePerKilometerFromConstructor;
+        this.tipPercentage = tipPercentageFromConstructor;
     }
 
     public double calculatePrice(int diameter) {
@@ -12,4 +16,18 @@ public class PizzaPriceCalculator {
         double pizzaArea = 3.14 * (radius) * (radius);
         return pizzaArea * priceForCm2;
     }
+
+    public double calculateDeliveryPrice(int distance) {
+        return distance * pricePerKilometer;
+    }
+
+    public double calculateTotalPrice(int diameter, int distance) {
+        double pizzaAndDelivery = calculatePrice(diameter) + calculateDeliveryPrice(distance);
+        return pizzaAndDelivery + calculateTip(pizzaAndDelivery);
+    }
+
+    public double calculateTip(double pizzaAndDelivery) {
+        return pizzaAndDelivery * tipPercentage;
+    }
+
 }
