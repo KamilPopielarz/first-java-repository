cc199fb9f3c6764dfc2db26b71f25b043f290ee7
Kamil <139891722+KamilPopielarz@users.noreply.github.com>
2023-10-10 21:01:48 +0200

    commented

--- java-project/src/Main.java
+++ java-project/src/Main.java
@@ -3,10 +3,10 @@ import service.PizzaPriceCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        PizzaPriceCalculator pizzaPriceCalculator = new PizzaPriceCalculator(0.04, 3.51, 0.05);
-        System.out.println("Pizza costs: " + pizzaPriceCalculator.calculatePrice(30) + " zł");
-        System.out.println("Delivery costs: " + pizzaPriceCalculator.calculateDeliveryPrice(10) + "zł");
-        System.out.println("OverallPrice: " + pizzaPriceCalculator.calculateTotalPrice(30, 10) + "zł");
+//        PizzaPriceCalculator pizzaPriceCalculator = new PizzaPriceCalculator(0.04, 3.51, 0.05);
+//        System.out.println("Pizza costs: " + pizzaPriceCalculator.calculatePrice(30) + " zł");
+//        System.out.println("Delivery costs: " + pizzaPriceCalculator.calculateDeliveryPrice(10) + "zł");
+//        System.out.println("OverallPrice: " + pizzaPriceCalculator.calculateTotalPrice(30, 10) + "zł");
 
         BurgerPriceCalculator burgerPriceCalculator = new BurgerPriceCalculator(3, 3, 0.05);
         System.out.println("Burger costs: " + burgerPriceCalculator.calculatePrice(10) + "zł");
