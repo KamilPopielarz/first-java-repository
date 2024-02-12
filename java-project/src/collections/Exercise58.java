2f1c6cacf64d94e75c0557e9597f8d43dfb91356
Kamil <139891722+KamilPopielarz@users.noreply.github.com>
2024-02-12 19:14:17 +0100

    Using Custom Objects in Sets and as Keys in Maps

---
+++ java-project/src/collections/Exercise58.java
@@ -0,0 +1,63 @@
+package collections;
+
+import java.util.*;
+
+class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "{ID is: " + id + " , " + "name is: " + name + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person person)) return false;
+        return id == person.id && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
+
+
+public class Exercise58 {
+    public static void main(String[] args) {
+
+        Person p1 = new Person(0, "Bob");
+        Person p2 = new Person(1, "Sue");
+        Person p3 = new Person(2, "Mike");
+        Person p4 = new Person(1, "Sue");
+
+
+        Map<Person, Integer> map = new HashMap<Person, Integer>();
+
+        map.put(p1, 1);
+        map.put(p2, 2);
+        map.put(p3, 3);
+        map.put(p4, 1);
+
+        for (Person key : map.keySet()) {
+            System.out.println(key + ":" + map.get(key));
+
+        }
+
+        Set<Person> set = new LinkedHashSet<>();
+
+        set.add(p1);
+        set.add(p2);
+        set.add(p3);
+        set.add(p4);
+
+        System.out.println(set);
+
+    }
+}
